fb1b38e1465f68859a0e41df76b690e8f0239db4
Kevin Piovezan <85972685+kevinpiovezan@users.noreply.github.com>
2021-10-21 01:26:30 -0300

    Delete dist directory

--- dist/src/config/auth.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    secret_token: '2eaa01c857f5ca96a4bb0126169bae30',
-    expires_in_token: '15m',
-    secret_refresh_token: 'cdbdd5e9d801ae62eedab0ca50047e24',
-    expires_in_refresh_token: '30d',
-    expires_in_refresh_token_days: 30,
-};
